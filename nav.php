310c9ad70ef0d9f26a3067bc9484e8d62b6b869b
Daniel Konatham <dako1569@gmail.com>
2025-02-11 18:15:15 -0600

    sign up buttons

--- nav.php
+++ nav.php
@@ -1,36 +1,36 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-<link href="path/to/your/custom/styles.css" rel="stylesheet"><link href="path/to/your/custom/styles.css" rel="stylesheet">
+<link href="path/to/your/custom/styles.css" rel="stylesheet">
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">lg navbar-light bg-light sticky-top">
+<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.html">Data Custom PCs</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span> class="navbar-toggler-icon"></span>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.html#about">About</a>a class="nav-link" href="index.html#about">About</a>
+                    <a class="nav-link" href="index.html#about">About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="index.html#services">Services</a>a class="nav-link" href="index.html#services">Services</a>
+                    <a class="nav-link" href="index.html#services">Services</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="shop.html">Shop</a>a class="nav-link" href="shop.html">Shop</a>
+                    <a class="nav-link" href="shop.html">Shop</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="contact.html">Contact</a>a class="nav-link" href="contact.html">Contact</a>
-                </li>/li>
-            </ul>ul>
-        </div>        </div>
+                    <a class="nav-link" href="contact.html">Contact</a>
+                </li>
+            </ul>
 
-        <!-- Sign Up & Sign In Buttons -->ns
-        <div class="d-flex ms-auto">
-            <a href="signup.html" class="button primary me-2">Sign Up</a>
-            <a href="signin.html" class="btn btn-outline-primary">Sign In</a> href="signin.html" class="btn btn-outline-primary">Sign In</a>
-        </div>div>
-    </div>div>
-</nav></nav>
+            <!-- Sign Up & Sign In Buttons -->
+            <div class="d-flex">
+                <a href="signup.html" class="btn btn-primary me-2">Sign Up</a>
+                <a href="signin.html" class="btn btn-outline-primary">Sign In</a>
+            </div>
+        </div>
+    </div>
+</nav>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
