0dfde26511c6ccdeb3fd747a4f13513e6d0a659e
Daniel Konatham <dako1569@gmail.com>
2025-02-12 01:01:59 -0600

    logo

--- nav.php
+++ nav.php
@@ -12,7 +12,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.html">Data Custom PCs</a>
+            <a class="navbar-brand" href="index.html"><img src="/DataCustomPCs/Assets/images/Logo.png">Data Custom PCs</img></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
