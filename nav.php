a1bf9a7c16cac0838eaa526172b65720306bc7ee
Daniel Konatham <dako1569@gmail.com>
2025-02-12 01:19:51 -0600

    updating logo

--- nav.php
+++ nav.php
@@ -6,7 +6,8 @@
     <title>Data Custom PCs</title>
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="path/to/your/custom/styles.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
 </head>
 <body>
 
