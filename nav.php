dc6eb0143b3e84dad77b8125ea116a0456a1bbe6
Daniel Konatham <dako1569@gmail.com>
2025-02-12 01:15:54 -0600

    updating logo

--- nav.php
+++ nav.php
@@ -12,7 +12,9 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.php"><img src="/Assets/images/Logo.png">Data Custom PCs</img></a>
+            <a class="navbar-brand" href="index.php">
+            <img src="/Assets/images/Logo.png" alt="Data Custom PCs Logo">
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
